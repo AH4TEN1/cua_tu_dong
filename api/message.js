dd330a0a8b030b1b9a68e2f7f75c80daf5328cd1
AH4TEN1 <hoanganhpham04102003@gmail.com>
2025-12-09 02:41:20 +0700

    fix2

--- api/message.js
+++ api/message.js
@@ -1,27 +1,30 @@
 // api/message.js
-let lastCommand = ''; // Lưu lệnh mới nhất từ Web UI
-let lastUpdated = 0; // Thời gian cập nhật gần nhất (ms)
+let lastCommand = '';
+let lastUpdated = 0;
 
-// Hàm trả JSON nhanh
-function send(res, msg) {
+const send = (res, obj) => {
   res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(JSON.stringify(msg));
-}
+  res.setHeader('Cache-Control', 'no-store'); // <<< NGĂN 304
+  res.status(200).send(JSON.stringify(obj));
+};
 
 export default function handler(req, res) {
-  const { cmd, get } = req.query;
+  const url = new URL(req.url, 'http://localhost');
 
-  // === Web UI gửi lệnh ===
+  const cmd = url.searchParams.get('cmd');
+  const get = url.searchParams.get('get');
+
+  // Web gửi lệnh
   if (cmd) {
     lastCommand = cmd;
     lastUpdated = Date.now();
     return send(res, { ok: true, received: cmd });
   }
 
-  // === ESP8266 lấy lệnh ===
-  if (get == '1') {
-    let temp = lastCommand;
-    lastCommand = ''; // ESP đọc xong thì xóa để tránh lặp
+  // ESP lấy lệnh
+  if (get === '1') {
+    const temp = lastCommand;
+    lastCommand = '';
     return send(res, {
       cmd: temp,
       updated: lastUpdated
